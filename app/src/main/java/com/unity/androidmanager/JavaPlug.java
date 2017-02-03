7948393ccaf5ac3f0288f1ed7a5a7aed8ea61b23
[developer119] <[developer119korea@gmail.com]>
2017-02-03 23:03:13 +0900

    Project Initialize

---
+++ app/src/main/java/com/unity/androidmanager/JavaPlug.java
@@ -0,0 +1,13 @@
+package com.unity.androidmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JavaPlug extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_java_plug);
+    }
+}
