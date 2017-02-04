fdcbe7ea50d3f8e558a7d9b662752ed175e41870
[developer119] <[developer119korea@gmail.com]>
2017-02-04 15:56:18 +0900

    구글 로그인 작업중

--- app/src/main/java/com/unity/androidmanager/JavaPlug.java
+++ app/src/main/java/com/unity/androidmanager/JavaPlug.java
@@ -1,13 +1,38 @@
 package com.unity.androidmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class JavaPlug extends AppCompatActivity {
 
+    private String _loginType;
+    private String _iapType;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_java_plug);
+    }
+
+    public void AndroidCall()
+    {
+
+    }
+
+    public void LoginCall(String type)
+    {
+        Intent loginIntent;
+
+        if(type == "Google")
+        {
+            loginIntent = new Intent(this, GoogleLogin.class);
+            startActivity(loginIntent);
+
+        }
+        else if(type == "Naver")
+        {
+
+        }
     }
 }
