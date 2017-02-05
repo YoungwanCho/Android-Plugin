0c695524419b2b1567ab026041ce225b494928e4
[developer119] <[developer119korea@gmail.com]>
2017-02-05 21:57:53 +0900

    네이버 로그인 기본 클래스 추가

---
+++ app/src/main/java/com/unity/androidmanager/NaverLogin.java
@@ -0,0 +1,134 @@
+package com.unity.androidmanager;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.nhn.android.naverlogin.OAuthLogin;
+import com.nhn.android.naverlogin.OAuthLoginDefine;
+import com.nhn.android.naverlogin.OAuthLoginHandler;
+import com.nhn.android.naverlogin.ui.view.OAuthLoginButton;
+
+
+/**
+ * Created by cho on 2017-02-05-0005.
+ */
+
+public class NaverLogin extends Activity {
+    private static final String TAG = "NaverLogin";
+    /**
+     * client 정보를 넣어준다.
+     */
+    private static String OAUTH_CLIENT_ID = "jyvqXeaVOVmV";
+    private static String OAUTH_CLIENT_SECRET = "527300A0_COq1_XV33cf";
+    private static String OAUTH_CLIENT_NAME = "네이버 아이디로 로그인";
+
+    private static OAuthLogin mOAuthLoginInstance;
+    private static Context mContext;
+    private static OAuthLogin mOAuthLoginInstance;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        OAuthLoginDefine.DEVELOPER_VERSION = true;
+        mContext = this;
+
+        initData();
+//        initView();
+
+//        this.setTitle("OAuthLoginSample Ver." + OAuthLogin.getVersion());
+    }
+
+    private void initData() {
+        mOAuthLoginInstance = OAuthLogin.getInstance();
+
+        mOAuthLoginInstance.init(mContext, OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET, OAUTH_CLIENT_NAME);
+		/*
+		 * 2015년 8월 이전에 등록하고 앱 정보 갱신을 안한 경우 기존에 설정해준 callback intent url 을 넣어줘야 로그인하는데 문제가 안생긴다.
+		 * 2015년 8월 이후에 등록했거나 그 뒤에 앱 정보 갱신을 하면서 package name 을 넣어준 경우 callback intent url 을 생략해도 된다.
+		 */
+        //mOAuthLoginInstance.init(mContext, OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET, OAUTH_CLIENT_NAME, OAUTH_callback_intent_url);
+    }
+
+    @Override
+    protected void onResume() {
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        super.onResume();
+
+
+    }
+    /**
+     * startOAuthLoginActivity() 호출시 인자로 넘기거나, OAuthLoginButton 에 등록해주면 인증이 종료되는 걸 알 수 있다.
+     */
+    static private OAuthLoginHandler mOAuthLoginHandler = new OAuthLoginHandler() {
+        @Override
+        public void run(boolean success) {
+            if (success) {
+                String accessToken = mOAuthLoginInstance.getAccessToken(mContext);
+                String refreshToken = mOAuthLoginInstance.getRefreshToken(mContext);
+                long expiresAt = mOAuthLoginInstance.getExpiresAt(mContext);
+                String tokenType = mOAuthLoginInstance.getTokenType(mContext);
+                mOauthAT.setText(accessToken);
+                mOauthRT.setText(refreshToken);
+                mOauthExpires.setText(String.valueOf(expiresAt));
+                mOauthTokenType.setText(tokenType);
+                mOAuthState.setText(mOAuthLoginInstance.getState(mContext).toString());
+            } else {
+                String errorCode = mOAuthLoginInstance.getLastErrorCode(mContext).getCode();
+                String errorDesc = mOAuthLoginInstance.getLastErrorDesc(mContext);
+                Toast.makeText(mContext, "errorCode:" + errorCode + ", errorDesc:" + errorDesc, Toast.LENGTH_SHORT).show();
+            }
+        };
+    };
+
+    private class DeleteTokenTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            boolean isSuccessDeleteToken = mOAuthLoginInstance.logoutAndDeleteToken(mContext);
+
+            if (!isSuccessDeleteToken) {
+                // 서버에서 token 삭제에 실패했어도 클라이언트에 있는 token 은 삭제되어 로그아웃된 상태이다
+                // 실패했어도 클라이언트 상에 token 정보가 없기 때문에 추가적으로 해줄 수 있는 것은 없음
+                Log.d(TAG, "errorCode:" + mOAuthLoginInstance.getLastErrorCode(mContext));
+                Log.d(TAG, "errorDesc:" + mOAuthLoginInstance.getLastErrorDesc(mContext));
+            }
+
+            return null;
+        }
+        protected void onPostExecute(Void v) {
+            updateView();
+        }
+    }
+
+    private class RequestApiTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            mApiResultText.setText((String) "");
+        }
+        @Override
+        protected String doInBackground(Void... params) {
+            String url = "https://openapi.naver.com/v1/nid/getUserProfile.xml";
+            String at = mOAuthLoginInstance.getAccessToken(mContext);
+            return mOAuthLoginInstance.requestApi(mContext, at, url);
+        }
+        protected void onPostExecute(String content) {
+            mApiResultText.setText((String) content);
+        }
+    }
+
+    private class RefreshTokenTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... params) {
+            return mOAuthLoginInstance.refreshAccessToken(mContext);
+        }
+        protected void onPostExecute(String res) {
+            updateView();
+        }
+    }
+}
